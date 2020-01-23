600d5835aba70814eac3a0e61178cf73e246c7a0
Henrique Vicentini <henrique@vicentini.us>
2020-01-22 21:50:45 -0800

    lesson 29.461 Embedded Server Configuration

---
+++ src/main/java/us/vicentini/sfgjms/config/ActiveMQEmbeddedServer.java
@@ -0,0 +1,48 @@
+package us.vicentini.sfgjms.config;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Slf4j
+@Component
+@Profile("local")
+public class ActiveMQEmbeddedServer {
+
+    private final ActiveMQServer server;
+
+
+    @SneakyThrows
+    public ActiveMQEmbeddedServer() {
+        log.info("Constructing!!!");
+        ConfigurationImpl configuration = new ConfigurationImpl()
+                .setPersistenceEnabled(false)
+                .setJournalDirectory("target/data/journal")
+                .setSecurityEnabled(false)
+                .addAcceptorConfiguration("invm", "vm://1");
+        server = ActiveMQServers.newActiveMQServer(configuration);
+    }
+
+
+    @SneakyThrows
+    @PostConstruct
+    public void init() {
+        log.info("Server starting!");
+        server.start();
+    }
+
+
+    @SneakyThrows
+    @PreDestroy
+    public void stop() {
+        log.info("Server stop!!!");
+        server.stop(true);
+    }
+}
